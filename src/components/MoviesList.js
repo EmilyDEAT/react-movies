fc423e0131fb33b896e23dfdbdd178aaa8774a11
Emily Déat <emily.deat@gmail.com>
2020-05-13 14:48:04 +0200

    create components Movie and MoviesList

---
+++ src/components/MoviesList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './MoviesList.css'
+
+const MoviesList = () => {
+  return (
+    <div className='moviesListContainer'>
+    </div>
+  )
+}
+
+export default MoviesList
