88bd792d3384d3ba81ea1fbb3d1caa83aea275b0
Emily Déat <emily.deat@gmail.com>
2020-05-13 20:20:58 +0200

    Add css on modal

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,30 +1,44 @@
 import React from "react";
 
-import cross from '../images/cross.png'
+import cross from "../images/cross.png";
 
 import "./MovieInfo.css";
 
 const MovieInfo = ({ hideInfo, movie, show }) => {
-
   // Time convert
   const timeConvert = (n) => {
-    const hours = Math.floor((n / 60));
-    const minutes = n - 60 * hours
-    const prettyMinutes = minutes < 10 ? `0${minutes}` : minutes
-    return `${hours}h${prettyMinutes}`
-    }
+    const hours = Math.floor(n / 60);
+    const minutes = n - 60 * hours;
+    const prettyMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${hours}h${prettyMinutes}`;
+  };
 
-  return movie === null ? "Loading" : (
+  return (
     <div className={`movieInfoContainer ${show ? "show" : "hide"}`}>
-      <div className="movieInfo">
-        <img className='movieInfoClose' src={cross} onClick={hideInfo} alt='close'/>
-        <img className='movieInfoImg' src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} alt={movie.title}/>
-        <h2>{movie.title}</h2>
-        <p>{movie.genres[0].name}</p>
-        <p>{timeConvert(movie.runtime)}</p>
-        <p>{movie.release_date}</p>
-        <p>{movie.overview}</p>
-      </div>
+      {movie === null ? (
+        "Loading"
+      ) : (
+        <div className="movieInfo">
+          <img
+            className="movieInfoClose"
+            src={cross}
+            onClick={hideInfo}
+            alt="close"
+          />
+          <img
+            className="movieInfoImg"
+            src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+            alt={movie.title}
+          />
+          <h2>{movie.title}</h2>
+          <div className='movieInfoFacts'>
+            <p className="movieInfoGenre">{movie.genres[0].name}</p>
+            <p className="movieInfoTime">{timeConvert(movie.runtime)}</p>
+            <p className="movieInfoDate">{movie.release_date}</p>
+          </div>
+          <p className="movieInfoOverview">{movie.overview}</p>
+        </div>
+      )}
     </div>
   );
 };
