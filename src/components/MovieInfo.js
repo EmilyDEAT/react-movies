b5153194830c0bd8b17244488754a1b344357abe
Emily Déat <emily.deat@gmail.com>
2020-05-14 11:16:43 +0200

    Add font and CSS on GameInfo

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -13,6 +13,14 @@ const MovieInfo = ({ hideInfo, movie, show }) => {
     return `${hours}h${prettyMinutes}`;
   };
 
+   // Date convert
+   const dateConvert = (s) => {
+     const day = s[8] + s[9]
+     const month = s[5] + s[6]
+     const year = s[0] + s[1] + s[2] + s[3]
+     return `${day}/${month}/${year}`
+   }
+
   return (
     <div className={`movieInfoContainer ${show ? "show" : "hide"}`}>
       {movie === null ? (
@@ -30,12 +38,13 @@ const MovieInfo = ({ hideInfo, movie, show }) => {
             src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
             alt={movie.title}
           />
-          <h2>{movie.title}</h2>
+          <h2 className='movieInfoTitle'>{movie.title}</h2>
           <div className='movieInfoFacts'>
             <p className="movieInfoGenre">{movie.genres[0].name}</p>
             <p className="movieInfoTime">{timeConvert(movie.runtime)}</p>
-            <p className="movieInfoDate">{movie.release_date}</p>
+            <p className="movieInfoDate">Sortie le {dateConvert(movie.release_date)}</p>
           </div>
+          <strong className="movieInfoTagline">{movie.tagline}</strong>
           <p className="movieInfoOverview">{movie.overview}</p>
         </div>
       )}
