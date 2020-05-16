a75ec6993fc124f60064b5a2273e0ae4b767bf53
Emily Déat <emily.deat@gmail.com>
2020-05-16 12:58:44 +0200

    Updates

---
+++ precache-manifest.a5895b0715844a1f9c7a96b9934936e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6368592d9814a1410023bff115fe0366",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "7d08631e9cf0c404716e",
+    "url": "/react-movies/static/css/main.511a5f4a.chunk.css"
+  },
+  {
+    "revision": "84ebe9a4c9f7e43f4e72",
+    "url": "/react-movies/static/js/2.d07ebacf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-movies/static/js/2.d07ebacf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d08631e9cf0c404716e",
+    "url": "/react-movies/static/js/main.d89a14df.chunk.js"
+  },
+  {
+    "revision": "0661bfa1bc4b6c21a0d3",
+    "url": "/react-movies/static/js/runtime-main.347e15d7.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/react-movies/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/react-movies/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "31782b075fc0568f3b314923c04e3720",
+    "url": "/react-movies/static/media/heart.31782b07.svg"
+  },
+  {
+    "revision": "522368bf2d68fcf08f203fa6c3a45678",
+    "url": "/react-movies/static/media/logo.522368bf.png"
+  }
+]);
